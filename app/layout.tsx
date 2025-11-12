5361b9589ad0771d867107f93600ce51ae48378e
xsdiablozb <cactusjackk48@gmail.com>
2025-11-12 16:33:24 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+          {children}
+        </div>
       </body>
     </html>
   );
